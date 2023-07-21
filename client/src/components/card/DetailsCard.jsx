397c1aab00125af05d4c61c6b8b7ab4510dd0752
sub1120 <subhamsahu1120@gmail.com>
2023-07-21 19:00:08 +0530

    fixed play button on details view

--- client/src/components/card/DetailsCard.jsx
+++ client/src/components/card/DetailsCard.jsx
@@ -1,5 +1,9 @@
 import { useRef } from "react";
-import { dislikeContent, likeContent } from "../../store/contentSlice";
+import {
+  addContentToWatchHistory,
+  dislikeContent,
+  likeContent,
+} from "../../store/contentSlice";
 import { useDispatch, useSelector } from "react-redux";
 import {
   AiFillDislike,
@@ -7,6 +11,7 @@ import {
   AiOutlineCloseCircle,
 } from "react-icons/ai";
 import { BsFillPlayFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const DetailsCard = ({
   contentId,
@@ -26,6 +31,7 @@ const DetailsCard = ({
 }) => {
   const userId = useSelector((state) => state.auth.userData._id);
   const videoRef = useRef(null);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const likeContentHanlder = () => {
@@ -36,15 +42,20 @@ const DetailsCard = ({
     dispatch(dislikeContent({ contentId, userId: userId }));
   };
 
+  const handlePlay = (contentId) => {
+    dispatch(addContentToWatchHistory({ contentId }));
+    navigate(`/watch/${contentId}`);
+  };
+
   return (
-    <div className="tranistion no-scrollbar relative mx-auto h-[500px] w-[90%] overflow-y-scroll rounded bg-netflix-black drop-shadow-lg duration-300 ease-in-out md:w-[650px]">
+    <div className="tranistion no-scrollbar relative mx-auto h-[500px] w-full overflow-y-scroll rounded bg-netflix-black drop-shadow-lg duration-300 ease-in-out md:w-[650px]">
       <div className="relative">
         {/* preview video*/}
         <div className="absolute -bottom-1 h-[25px] w-full bg-gradient-to-b from-netflix-black/0 to-netflix-black/100 md:h-[100px] lg:h-[150px]"></div>
         <div className="w-full">
           <video
             ref={videoRef}
-            className="h-[350px] w-full rounded-tl rounded-tr object-cover"
+            className="h-[250px] w-full rounded-tl rounded-tr object-cover md:h-[300px] lg:h-[350px]"
             poster={thumbnailURL}
             src={trailerUrl}
             loop
@@ -54,7 +65,7 @@ const DetailsCard = ({
 
         {/* modal close button */}
         <button
-          className="absolute right-2 top-2 cursor-pointer"
+          className="absolute right-4 top-4 cursor-pointer"
           onClick={handleClose}
         >
           <AiOutlineCloseCircle className="text-4xl text-white" />
@@ -62,10 +73,14 @@ const DetailsCard = ({
 
         {/* hero text */}
         <div className="absolute bottom-6 left-6 flex cursor-pointer items-center gap-2 md:bottom-8 md:left-12">
-          <button className="flex cursor-pointer items-center gap-2 rounded bg-white px-2 py-1 text-sm font-semibold text-black md:px-4 md:text-lg ">
+          <button
+            className="flex cursor-pointer items-center gap-2 rounded bg-white px-2 py-1 text-sm font-semibold text-black md:px-4 md:text-lg "
+            onClick={() => handlePlay(contentId)}
+          >
             <BsFillPlayFill className="text-2xl lg:text-4xl" />
             Play
           </button>
+
           <button
             onClick={likeContentHanlder}
             className="cursor-pointer rounded-full border-2 border-white p-[0.35rem] "
